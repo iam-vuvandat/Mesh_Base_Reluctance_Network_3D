e961233b0409ea7ef17a5e3ec0591611161b655e
iam-vuvandat <iam.vuvandat@gmail.com>
2025-12-10 17:09:03 +0700

    Thêm vào class từ thế nút MagneticPotential

--- core_class/models/Element.py
+++ core_class/models/Element.py
@@ -8,7 +8,7 @@ class Element:
                  mesh = None,
                  loop_flux = None,
                  winding_current = None):
-        
+        self.position = position
         self.material_database = motor.material_database
         self.position = position
         self.loop_flux = loop_flux
@@ -18,5 +18,23 @@ class Element:
                                     geometry=geometry,
                                     mesh=mesh,
                                     )
-        
+        # material
         self.material = info.material
+
+        # segment dimension
+        self.segment_r_length = info.segment_r_length
+        self.segment_t_length = info.segment_t_length
+        self.segment_t_length = info.segment_t_length
+
+        # element dimension
+        self.element_r_length = info.element_r_length
+        self.element_t_length = info.element_t_length
+        self.element_z_length = info.element_z_length
+
+        # magnet properties
+        self.segment_magnet_source = info.magnet_source
+        self.magnetization_direction = info.magnetization_direction
+
+        # winding properties
+        self.segment_winding_vector = info.winding_vector
+        self.winding_normal = info.winding_normal
