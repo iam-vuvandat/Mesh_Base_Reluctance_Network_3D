56c54fe81763df054d0b2be83e9a9e1fed485654
iam-vuvandat <iam.vuvandat@gmail.com>
2025-12-13 23:35:30 +0700

    #

--- solver/core/create_magnetic_potential_equation.py
+++ solver/core/create_magnetic_potential_equation.py
@@ -1,8 +1,21 @@
-
+import numpy as np
+import scipy.sparse as sp
 
 
 def create_magnetic_potential_equation(reluctance_network,
                                        use_minimum_reluctance = False):
+    # Phương trình có dạng G * U = J 
+    matrix_size = reluctance_network.mesh.total_cells -1 # nút cuối cùng tham chiếu = 0
+    G = [[],[],[]]
+    J = np.zeros(matrix_size)
     
-    return []
+    for i in range(matrix_size):
+        
+
+
+
+
+
+        1
+    return 1
 
